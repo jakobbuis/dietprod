2fe2f0c9cd187a654939e13d9f068dbffc6032f2
Jakob Buis <jakob@jakobbuis.nl>
2018-07-21 13:24:26 +0200

    reminder sent on specific moment of day, using Carbon

--- reminder.php
+++ reminder.php
@@ -1,11 +1,24 @@
 <?php
 
+use Carbon\Carbon;
+use Twilio\Rest\Client;
+
 require __DIR__ . '/vendor/autoload.php';
 
 $config = require __DIR__ . '/config.php';
-$twilio = new \Twilio\Rest\Client($config['twilio']['sip'], $config['twilio']['token']);
+$twilio = new Client($config['twilio']['sip'], $config['twilio']['token']);
 
 foreach ($config['clients'] as $client) {
+    // Determine if it's time for a message
+    $moment = Carbon::now()->format('Y-m-d')  . ' '  . $client['moment'];
+    $now = Carbon::now()->format('Y-m-d H:i');
+    if ($moment !== $now) {
+        echo 'Not sending message to '.$client['number'] . PHP_EOL;
+        continue;
+    }
+
+    // Send a message
+    echo 'Sending message to '.$client['number'] . PHP_EOL;
     $message = $client['messages'][array_rand($client['messages'])];
     $twilio->messages->create($client['number'], [
         'from' => $config['twilio']['from_number'],
